d09082e2b96bfcadefd36feb3fa6df0ecf34e024
RUBIUS\chebanovdd <dmitriy.chebanov@rubius.com>
2021-04-22 18:22:31 +0700

    Added sizer and device_preview packages.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:sizer/sizer.dart';
 
 import 'logic/stores/counter_store.dart';
 import 'logic/stores/theme_store.dart';
 import 'material_app.dart';
 
-void main() => runApp(const App());
+void main() => runApp(DevicePreview(enabled: false, builder: (_) => const App()));
 
 class App extends StatelessWidget {
   const App();
@@ -18,7 +20,9 @@ class App extends StatelessWidget {
         Provider<CounterStore>(create: (_) => CounterStore())
       ],
       child: Consumer<ThemeStore>(
-          builder: (_, store, __) => CounterApp(themeStore: store)
+        builder: (_, store, __) => Sizer(
+          builder: (context, orientation, screenType) => CounterApp(themeStore: store)
+        ),
       ),
     );
   }
