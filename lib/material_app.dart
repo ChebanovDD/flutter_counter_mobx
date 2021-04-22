2ae15039e6d365dbbfaedfcd62b70b4f61d19630
RUBIUS\chebanovdd <dmitriy.chebanov@rubius.com>
2021-04-22 17:57:40 +0700

    Moved CounterApp widget to material_app file.

---
+++ lib/material_app.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+
+import 'core/constants/strings.dart';
+import 'core/themes/app_theme.dart';
+import 'logic/stores/theme_store.dart';
+import 'presentation/router/app_router.dart';
+
+class CounterApp extends StatefulWidget {
+  const CounterApp({@required this.themeStore});
+
+  final ThemeStore themeStore;
+
+  @override
+  _CounterAppState createState() => _CounterAppState();
+}
+
+class _CounterAppState extends State<CounterApp> with WidgetsBindingObserver {
+  @override
+  void initState() {
+    WidgetsBinding.instance.addObserver(this);
+    super.initState();
+  }
+
+  @override
+  void didChangePlatformBrightness() {
+    widget.themeStore.updateAppTheme();
+    super.didChangePlatformBrightness();
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Observer(
+      builder: (_) => MaterialApp(
+        title: Strings.appTitle,
+        theme: AppTheme.lightTheme,
+        darkTheme: AppTheme.darkTheme,
+        themeMode: widget.themeStore.themeMode,
+        debugShowCheckedModeBanner: false,
+        initialRoute: AppRouter.initialRoute,
+        onGenerateRoute: AppRouter.onGenerateRoute,
+      ),
+    );
+  }
+}
